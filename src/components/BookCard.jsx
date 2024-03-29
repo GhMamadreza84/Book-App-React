add742dbe640d6a0a074248650df4ef989dba35d
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-29 19:36:47 +0300

    add like state to project

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,5 +1,7 @@
 import { AiFillHeart } from "react-icons/ai";
+import { useState } from "react";
 const BookCard = ({ data: { title, author, pages, image, language } }) => {
+  const [like, setLike] = useState(false);
   return (
     <div>
       <img src={image} alt={title} />
@@ -12,7 +14,7 @@ const BookCard = ({ data: { title, author, pages, image, language } }) => {
         </div>
       </div>
       <button>
-        <AiFillHeart />
+        <AiFillHeart color="red" fontSize="1.8rem" />
       </button>
     </div>
   );
