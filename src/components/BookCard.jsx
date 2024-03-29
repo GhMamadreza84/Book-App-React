5ace9d5cfecb8ded19da4fb617952ea579fb6126
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-29 19:30:12 +0300

    create BooksCard in project

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,5 +1,18 @@
-const BookCard = ({ data: { title, author, pages, images, language } }) => {
-  return <></>;
+const BookCard = ({ data: { title, author, pages, image, language } }) => {
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <div>
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <div>
+          <span>{language}</span>
+          <span>{pages} Pages</span>
+        </div>
+      </div>
+      <button>like</button>
+    </div>
+  );
 };
 
 export default BookCard;
