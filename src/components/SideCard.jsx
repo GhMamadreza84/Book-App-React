604f97b51434c9cfdc97000d04e6d6aed796b5e6
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-31 18:02:04 +0300

    add image and title to sidecard

--- src/components/SideCard.jsx
+++ src/components/SideCard.jsx
@@ -1,5 +1,8 @@
-const SideCard = ( data: { image, title } ) => {
-  return <div></div>;
+const SideCard = ( {data: { image, title }} ) => {
+  return <div>
+    <img src={image} alt={title} />
+    <p>{title}</p>
+  </div>;
 };
 
 export default SideCard;
